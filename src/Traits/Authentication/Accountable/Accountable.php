bca28958ee545affa8678a76c31378dfccceeae9
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-07 02:24:58 +0300

    Enhance

--- src/Traits/Authentication/Accountable/Accountable.php
+++ src/Traits/Authentication/Accountable/Accountable.php
@@ -2,6 +2,15 @@
 
 namespace Raid\Core\Auth\Traits\Authentication\Accountable;
 
+use Raid\Core\Auth\Models\Authentication\Contracts\AccountInterface;
+
 trait Accountable
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function findAccount(string $key, mixed $value): ?AccountInterface
+    {
+        return $this->where($key, $value)->first();
+    }
 }
