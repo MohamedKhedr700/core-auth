3bbd86273d40d5ad6c047d13cd3cb80e16092346
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-07 02:25:07 +0300

    Enhance

--- src/Models/Authentication/Accountable.php
+++ src/Models/Authentication/Accountable.php
@@ -5,7 +5,7 @@ namespace Raid\Core\Auth\Models\Authentication;
 use Raid\Core\Auth\Models\Authentication\Contracts\AccountableInterface;
 use Raid\Core\Auth\Traits\Authentication\Accountable\Accountable as AccountableTrait;
 
-abstract class Accountable implements AccountableInterface
+class Accountable implements AccountableInterface
 {
     use AccountableTrait;
 }
