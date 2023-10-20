b1a80b4a3be6c0a97b5c16e087d5adf54cdb0af9
mohamedkhedr700 <M.khedr@rowaad.net>
2023-10-21 02:19:43 +0300

    Enhance

--- src/Traits/Model/Authenticatable.php
+++ src/Traits/Model/Authenticatable.php
@@ -10,38 +10,42 @@ trait Authenticatable
 {
     /**
      * Attempt to authenticate with credentials.
+     *
+     * @throws InvalidAuthenticatorException
      */
     public static function attempt(array $credentials, string $channel = null): AuthChannelInterface
     {
         $authenticator = static::getAuthenticator();
+
+        if (! $authenticator) {
+            throw new InvalidAuthenticatorException('No authenticator is defined for '.static::class);
+        }
 
         return $authenticator::attempt($credentials, $channel);
     }
 
     /**
      * Login with an account model.
+     *
+     * @throws InvalidAuthenticatorException
      */
     public static function login(AccountInterface $account, string $channel = null): AuthChannelInterface
     {
         $authenticator = static::getAuthenticator();
+
+        if (! $authenticator) {
+            throw new InvalidAuthenticatorException('No authenticator is defined for '.static::class);
+        }
 
         return $authenticator::login($account, $channel);
     }
 
     /**
      * Get authenticator.
-     *
-     * @throws InvalidAuthenticatorException
      */
     public static function getAuthenticator(): string
     {
-        $authenticator = config('authentication.authenticators.'.static::class);
-
-        if (! $authenticator) {
-            throw new InvalidAuthenticatorException('No authenticator is defined for '.static::class);
-        }
-
-        return $authenticator;
+        return config('authentication.authenticators.'.static::class);
     }
 
     /**
